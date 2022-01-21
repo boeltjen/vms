3510e6103d50b7894d832759b285cd436a2d2955
boeltjen <16950666+boeltjen@users.noreply.github.com>
2022-01-21 13:08:49 -0500

    Update printBadgeBookmarklet.js

--- printBadgeBookmarklet.js
+++ printBadgeBookmarklet.js
@@ -2,11 +2,12 @@ javascript: (function() {
 	/* uncomment per version to use */
 	/* const printBadgeId = "printBadgeInfo" + "FromCard"; */
 	const printBadgeId = "printBadgeInfo" + "FromApi";
+	const rootPath = "https://boeltjen.github.io";
 	
 	if(!(document.getElementById(printBadgeId+"Script"))){
 		var printBadgeScript=document.createElement("script");
 		printBadgeScript.id = printBadgeId+"Script";
-		file_var = "https://boeltjen.github.io/vms/"+printBadgeId.toLowerCase()+".js?nocache="+(new Date()).getTime();
+		file_var = rootPath + "/vms/"+printBadgeId.toLowerCase()+".js?nocache="+(new Date()).getTime();
 		printBadgeScript.src=file_var;
 		printBadgeScript.type="text/javascript";
 		document.head.appendChild(printBadgeScript);
